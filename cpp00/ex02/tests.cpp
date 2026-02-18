0fc390d281a8c772b4a4e4fb4238d37bbc1a9c7b
Gun$hoot <128548165+Gun8hoot@users.noreply.github.com>
2026-02-18 18:32:39 +0100

    Update tests.cpp

--- cpp00/ex02/tests.cpp
+++ cpp00/ex02/tests.cpp
@@ -27,7 +27,7 @@ int		main( void ) {
 
 	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
 	size_t const		d_size( sizeof(d) / sizeof(int) );
-	ints_t				deposits( d, d + d_size );
+	ints_t				deposits( d, d + d_size );     // https://cplusplus.com/reference/vector/vector/vector/
 	ints_t::iterator	dep_begin	= deposits.begin();
 	ints_t::iterator	dep_end		= deposits.end();
 
