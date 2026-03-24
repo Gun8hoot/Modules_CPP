d23603c96c62cc3f4ff774323679e1234b2f4d7f
gun8hoot <gun8hoot@proton.me>
2026-03-24 08:43:39 +0100

    Finish 02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -8,8 +8,7 @@ int Account::_totalNbWithdrawals = 0;
 /* ----- PUBLIC ----- */
 int	Account::getNbAccounts( void )
 {
-	int ret = Account::_nbAccounts;
-	return (ret);
+	return (Account::_nbAccounts);
 }
 
 int	Account::getTotalAmount( void )
@@ -30,51 +29,74 @@ int	Account::getNbWithdrawals( void )
 void	Account::displayStatus( void ) const
 {
 	Account::_displayTimestamp();
-	std::cout << " index:" << this->_accountIndex << ";amount:" <<  this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+	std::cout	<< " index:" << this->_accountIndex
+				<< ";amount:" <<  this->_amount
+				<< ";deposits:" << this->_nbDeposits
+				<< ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
 
 void	Account::displayAccountsInfos( void )
 {
 	Account::_displayTimestamp();
-	std::cout << " account:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+	std::cout	<< " account:" << _nbAccounts
+				<< ";total:" << _totalAmount
+				<< ";deposits:" << _totalNbDeposits
+				<< ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 Account::Account( int initial_deposit )
 {
-	this->_accountIndex = getNbAccounts() + 1;
+	this->_accountIndex = getNbAccounts();
 	_nbAccounts++;
 	this->_amount = initial_deposit;
 	_totalAmount += initial_deposit;
 	this->_displayTimestamp();
-	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+	std::cout	<< " index:" << this->_accountIndex
+				<< ";amount:" << this->_amount
+				<< ";created" << std::endl;
 }
 
 Account::~Account(void)
 {
 	this->_displayTimestamp();
-	std::cout << " index:" << this->_accountIndex <<_nbWithdrawals << ";amount:" << this->_amount << ";closed" << std::endl;
+	std::cout	<< " index:" << this->_accountIndex
+				<< ";amount:" << this->_amount
+				<< ";closed" << std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
 {
-	std::cout << " index:" << this->_accountIndex <<_nbWithdrawals << ";amount:" << this->_amount << ";closed" << std::endl;
+	this->_nbDeposits++;
+	_totalNbDeposits++;
+	this->_displayTimestamp();
+	std::cout	<< " index:" << this->_accountIndex
+				<< ";p_amount:" << this->_amount
+				<< ";amount:" << this->_amount + deposit
+				<< ";nb_deposits:" << this->_nbDeposits << std::endl;
 	this->_amount = this->_amount + deposit;
-	_nbDeposits++;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
+	this->_displayTimestamp();
 	if (this->_amount < withdrawal)
 	{
-		std::cout << " index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawals:refused" << std::endl;
+		std::cout	<< " index:" << this->_accountIndex
+					<< ";p_amount:" << this->_amount
+					<< ";withdrawals:refused" << std::endl;
 		return (false);
 	}
 	else
 	{
-		std::cout << " index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawals:" << this->_accountIndex + withdrawal << ";amount:" << this->_amount << ";nb_withdrawals:" << _totalNbWithdrawals << std::endl;
- 		this->_amount = this->_amount - withdrawal;
  		_nbWithdrawals++;
+   		_totalNbWithdrawals++;
+		std::cout	<< " index:" << this->_accountIndex
+					<< ";p_amount:" << this->_amount
+					<< ";withdrawals:" << withdrawal
+					<< ";amount:" << this->_amount - withdrawal
+					<< ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+ 		this->_amount = this->_amount - withdrawal;
  		return (true);
 	}
 }
@@ -87,15 +109,19 @@ int		Account::checkAmount( void ) const
 
 /* ----- PRIVATE ----- */
 
-void	Account::_displayTimestamp( void )
+void Account::_displayTimestamp(void)
 {
 	std::time_t time = std::time(0);
-
 	std::tm *time_vars = std::localtime(&time);
 
 	std::cout << "[";
-	std::cout << time_vars->tm_year << time_vars->tm_mon << time_vars->tm_mday;
+	std::cout << std::setfill('0');
+	std::cout << (time_vars->tm_year + 1900)
+	          << std::setw(2) << (time_vars->tm_mon + 1)
+	          << std::setw(2) << time_vars->tm_mday;
 	std::cout << "_";
-	std::cout << time_vars->tm_hour << time_vars->tm_min << time_vars->tm_sec;
+	std::cout << std::setw(2) << time_vars->tm_hour
+	          << std::setw(2) << time_vars->tm_min
+	          << std::setw(2) << time_vars->tm_sec;
 	std::cout << "]";
 }
